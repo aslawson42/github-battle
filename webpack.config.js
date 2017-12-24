98508db27e78381b6d498fecf3a2439f51451e3e
Drew <andrewslawson42@gmail.com>
2017-12-24 17:11:12 -0500

    Build for Production and Hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
   entry: './app/index.js',
+  //This is where the compiled code outputs to when we go to production
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
@@ -17,7 +19,26 @@ module.exports = {
   devServer: {
     historyApiFallback: true
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'app/index.html'
-  })]
+  //Creates index.html file in dist folder
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
+
+//Tells React that we're building for production
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      //This script defines NODE_ENV as 'production' in the actual compiled code
+      //The 'build' command in package.json defines NODE_ENV only in this webpack.config.js file
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
 }
+
+module.exports = config;
